1f6a97c8c8f9d14b43735625fd07ebc737b0ec09
Hong Sheng Tan <skytanhongsheng@gmail.com>
2024-11-19 16:18:21 +0800

    Basic Validations Created

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,10 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :accommodation
 
+  STATUS = ["Pending", "Accepted", "Declined"]
+
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :guest_count, presence: true
+  validates :status, presence: true, inclusion: STATUS
 end
