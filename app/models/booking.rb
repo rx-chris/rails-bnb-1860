dceb3926794392a8607d891e2b920d69e5dd582e
Hong Sheng Tan <skytanhongsheng@gmail.com>
2024-11-19 16:07:00 +0800

    Basic Validations Added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :accommodation
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :guest_count, presence: true
 end
