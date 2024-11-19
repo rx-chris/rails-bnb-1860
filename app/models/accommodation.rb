5867b4bcc8b85782b82d3beee353bbd390ea226f
Christopher Chua <rxchris94@gmail.com>
2024-11-19 18:16:01 +0800

    fix(model): Fixed the model validations

--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -3,7 +3,7 @@ class Accommodation < ApplicationRecord
 
   TYPES_OF_PLACE = ["Room", "Entire home"]
 
-  validates :type_of_place, presence: true, uniqueness: true, inclusion: { in: TYPES_OF_PLACE }
+  validates :type_of_place, presence: true, inclusion: { in: TYPES_OF_PLACE }
   validates :price, presence: true
   validates :address, presence: true
   validates :bed_count, presence: true
