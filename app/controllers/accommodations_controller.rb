ea02c47596daa2a4312592be66706dac72395e4a
Tirta Swari <tirtasafariman@gmail.com>
2024-11-20 14:47:56 +0800

    updated front end of accommodations#show

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -1,5 +1,7 @@
 class AccommodationsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+
   def show
-    @accommodation = @Accommodation.find[params[:id]]
+    @accommodation = Accommodation.find(params[:id])
   end
 end
