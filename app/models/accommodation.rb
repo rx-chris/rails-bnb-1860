e0422b0b3e2a6908aa817657f0bcdcdfff715703
Hong Sheng Tan <skytanhongsheng@gmail.com>
2024-11-21 13:10:43 +0800

    Add accommodation form completed

--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -10,7 +10,8 @@ class Accommodation < ApplicationRecord
   TYPES_OF_PLACE = ["Room", "Entire home"]
 
   validates :type_of_place, presence: true, inclusion: { in: TYPES_OF_PLACE }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :title, presence: true, uniqueness: true
+  validates :price, presence: true
   validates :address, presence: true
   validates :bed_count, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :bedroom_count, presence: true, numericality: { greater_than_or_equal_to: 0 }
