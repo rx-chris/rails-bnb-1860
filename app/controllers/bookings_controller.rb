598ce50d95b20a948a2668c73cd7c2bf2ae94d59
Tirta Swari <tirtasafariman@gmail.com>
2024-11-25 15:13:12 +0800

    added tabs on bookings page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,9 @@
 class BookingsController < ApplicationController
   def index
-    @pending_bookings = current_user.booking_requests.where(status: 'Pending')
+    @pending_booking_requests = current_user.booking_requests.where(status: 'Pending')
+    @pending_bookings = current_user.bookings.where(status: 'Pending')
   end
+
 
   def accept
     @booking = Booking.find(params[:id])
